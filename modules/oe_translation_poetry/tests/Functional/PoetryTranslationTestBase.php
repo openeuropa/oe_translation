6fe9da28e990305138716d1e2b9a03ee9c8a2777
D Vargas <hiphipvargas@sapo.pt>
2019-12-06 03:34:56 +0100

    OPENEUROPA-2252: Add test for translation update request.

--- modules/oe_translation_poetry/tests/Functional/PoetryTranslationTestBase.php
+++ modules/oe_translation_poetry/tests/Functional/PoetryTranslationTestBase.php
@@ -9,6 +9,7 @@ use Drupal\node\NodeInterface;
 use Drupal\oe_translation_poetry_mock\PoetryMock;
 use Drupal\Tests\oe_translation\Functional\TranslationTestBase;
 use Drupal\Tests\oe_translation_poetry\Traits\PoetryTestTrait;
+use Drupal\tmgmt\Entity\Job;
 
 /**
  * Base class for functional tests of the Poetry integration.
@@ -116,6 +117,77 @@ class PoetryTranslationTestBase extends TranslationTestBase {
 
     $this->drupalPostForm($node->toUrl('drupal:content-translation-overview'), $values, 'Request DGT translation for the selected languages');
     $this->assertSession()->pageTextContains($expected_title->__toString());
+  }
+
+  /**
+   * Creates jobs that mimic a request having been made to Poetry.
+   *
+   * @param array $values
+   *   The content values.
+   * @param array $languages
+   *   The job languages.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The created node.
+   */
+  protected function createNodeWithRequestedJobs(array $values, array $languages = []): NodeInterface {
+    /** @var \Drupal\node\NodeStorageInterface $node_storage */
+    $node_storage = $this->entityTypeManager->getStorage('node');
+
+    /** @var \Drupal\node\NodeInterface $node */
+    $node = $node_storage->create([
+      'type' => 'page',
+    ] + $values);
+    $node->save();
+
+    // Create a job for given languages to mimic that the request has been
+    // made to Poetry.
+    foreach ($languages as $language) {
+      $job = tmgmt_job_create('en', $language, 0);
+      $job->translator = 'poetry';
+      $job->addItem('content', 'node', $node->id());
+      $job->set('poetry_request_id', $this->defaultIdentifierInfo);
+      $job->set('state', Job::STATE_ACTIVE);
+      $date = new \DateTime('05/04/2019');
+      $job->set('poetry_request_date', $date->format('Y-m-d\TH:i:s'));
+      $job->save();
+    }
+
+    // Ensure the jobs do not contain any info related to Poetry status.
+    $this->jobStorage->resetCache();
+    /** @var \Drupal\tmgmt\JobInterface[] $jobs */
+    $jobs = $this->jobStorage->loadMultiple();
+    foreach ($jobs as $job) {
+      $this->assertTrue($job->get('poetry_request_date_updated')->isEmpty());
+      $this->assertTrue($job->get('poetry_state')->isEmpty());
+    }
+
+    return $node;
+  }
+
+  /**
+   * Submits the request in queue.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node.
+   */
+  protected function submitRequestInQueue(NodeInterface $node): void {
+    // Submit the request form.
+    $date = new \DateTime();
+    $date->modify('+ 7 days');
+    $values = [
+      'details[date]' => $date->format('Y-m-d'),
+      'details[contact][auteur]' => 'userid',
+      'details[contact][secretaire]' => 'userid',
+      'details[contact][contact]' => 'userid',
+      'details[contact][responsable]' => 'userid',
+      'details[organisation][responsible]' => 'DIGIT',
+      'details[organisation][author]' => 'IE/CE/DIGIT',
+      'details[organisation][requester]' => 'IE/CE/DIGIT',
+    ];
+    $this->drupalPostForm(NULL, $values, 'Send request');
+    $this->assertSession()->pageTextContains('The request has been sent to DGT.');
+    $this->assertSession()->addressEquals('/en/node/' . $node->id() . '/translations');
   }
 
 }
