a6325a37170806fec87bcc5de929a81613189f63
Gilmar Lima <gilmar.lima@novacomm-europa.eu>
2021-06-24 15:21:36 +0000

    EWPP-917: Fix Parser::outerHtml comentary.

--- src/Controller/ContentTranslationController.php
+++ src/Controller/ContentTranslationController.php
@@ -60,9 +60,9 @@ class ContentTranslationController extends BaseContentTranslationController {
    *   The language manager.
    */
   public function __construct(ContentTranslationManagerInterface $manager, EntityTypeManagerInterface $entity_type_manager, TranslatorManager $translator_manager, EventDispatcherInterface $event_dispatcher, LanguageManagerInterface $language_manager = NULL) {
-    //$entity_field_manager = \Drupal::service('entity_field.manager');
+    $entity_field_manager = \Drupal::service('entity_field.manager');
     
-    parent::__construct($manager);
+    parent::__construct($manager, $entity_field_manager);
 
     $this->entityTypeManager = $entity_type_manager;
     $this->translatorManager = $translator_manager;
