a0b3dcc82f1d7db799e1c0cc57c01939c5dbc101
upchuk <danny@webomelette.com>
2023-03-08 15:42:22 +0100

    EWPP-0000: Fixing client authentication variables.

--- modules/oe_translation_epoetry/src/ClientCertificateAuthentication.php
+++ modules/oe_translation_epoetry/src/ClientCertificateAuthentication.php
@@ -22,7 +22,7 @@ class ClientCertificateAuthentication extends OriginalClientCertificateAuthentic
     $service_url = Settings::get('epoetry.auth.cert_service_url') ? Settings::get('epoetry.auth.cert_service_url') : '';
     $cert_file_path = Settings::get('epoetry.auth.cert_path', '') ? Settings::get('epoetry.auth.cert_path', '') : '';
     $cert_password = Settings::get('epoetry.auth.cert_password', '') ? Settings::get('epoetry.auth.cert_password', '') : '';
-    $eu_login_base_path = Settings::get('epoetry.auth.eulogin_base_path', '') ? Settings::get('epoetry.auth.cert_password', '') : '';
+    $eu_login_base_path = Settings::get('epoetry.auth.eulogin_base_path', '') ? Settings::get('epoetry.auth.eulogin_base_path', '') : '';
     parent::__construct($service_url, $cert_file_path, $cert_password, $eu_login_base_path, $loggerChannelFactory->get('oe_translation_epoetry'));
   }
 
