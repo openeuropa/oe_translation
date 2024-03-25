27b46236171311cb365692b6e8a6b37ecca2ee2b
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-03-25 08:43:42 +0100

    D8AGE-286: Refactored requirements test.

--- modules/oe_translation_cdt/tests/src/Kernel/RequirementsTest.php
+++ modules/oe_translation_cdt/tests/src/Kernel/RequirementsTest.php
@@ -22,140 +22,44 @@ class RequirementsTest extends TranslationKernelTestBase {
 
   /**
    * Tests that the CDT requirements values are set.
-   *
-   * @param string[] $settings
-   *   The settings to test.
-   * @param mixed[] $expected_requirements
-   *   The expected requirements.
-   *
-   * @dataProvider cdtRequirementsProvider
    */
-  public function testCdtRequirements(array $settings, array $expected_requirements): void {
+  public function testCdtRequirements(): void {
+    // Test correct settings.
+    $settings = [
+      'cdt.token_api_endpoint' => 'https://example.com/token',
+      'cdt.main_api_endpoint' => 'https://example.com/v2/CheckConnection',
+      'cdt.reference_data_api_endpoint' => 'https://example.com/v2/requests/businessReferenceData',
+      'cdt.validate_api_endpoint' => 'https://example.com/v2/requests/validate',
+      'cdt.requests_api_endpoint' => 'https://example.com/v2/requests',
+      'cdt.identifier_api_endpoint' => 'https://example.com/v2/requests/requestIdentifierByCorrelationId/:correlationId',
+      'cdt.status_api_endpoint' => 'https://example.com/v2/requests/:requestyear/:requestnumber',
+      'cdt.username' => 'test_username',
+      'cdt.client' => 'test_client',
+      'cdt.password' => 'test_password',
+      'cdt.api_key' => 'test_api_key',
+    ];
     new Settings($settings);
     \Drupal::moduleHandler()->loadInclude('oe_translation_cdt', 'install');
     $requirements = oe_translation_cdt_requirements('runtime');
-
-    $this->assertCount(10, $requirements);
-    foreach ($requirements as $name => $requirement) {
-      $this->assertNotEmpty($expected_requirements[$name], sprintf('The %s requirement is not expected', $name));
-      $expected_requirement = $expected_requirements[$name];
-      $this->assertEquals($expected_requirement['value'], $requirement['value'], sprintf('The %s value is incorrect', $name));
-      $this->assertEquals($expected_requirement['severity'], $requirement['severity'], sprintf('The %s severity is incorrect', $name));
+    foreach ($settings as $name => $value) {
+      $this->assertArrayHasKey($name, $requirements);
+      if (in_array($name, ['cdt.password', 'cdt.api_key'])) {
+        $this->assertEquals('Value set', $requirements[$name]['value']);
+      }
+      else {
+        $this->assertEquals($value, $requirements[$name]['value']);
+      }
+      $this->assertEquals(0, $requirements[$name]['severity']);
     }
-  }
 
-  /**
-   * Data provider for testCdtRequirements.
-   *
-   * @return mixed[]
-   *   The test data.
-   */
-  public static function cdtRequirementsProvider(): array {
-    return [
-      'correct settings' => [
-        [
-          'cdt.token_api_endpoint' => 'https://example.com/token',
-          'cdt.main_api_endpoint' => 'https://example.com/v2/CheckConnection',
-          'cdt.reference_data_api_endpoint' => 'https://example.com/v2/requests/businessReferenceData',
-          'cdt.validate_api_endpoint' => 'https://example.com/v2/requests/validate',
-          'cdt.requests_api_endpoint' => 'https://example.com/v2/requests',
-          'cdt.identifier_api_endpoint' => 'https://example.com/v2/requests/requestIdentifierByCorrelationId/:correlationId',
-          'cdt.status_api_endpoint' => 'https://example.com/v2/requests/:requestyear/:requestnumber',
-          'cdt.username' => 'test_username',
-          'cdt.client' => 'test_client',
-          'cdt.password' => 'test_password',
-        ],
-        [
-          'cdt.token_api_endpoint' => [
-            'value' => 'https://example.com/token',
-            'severity' => 0,
-          ],
-          'cdt.main_api_endpoint' => [
-            'value' => 'https://example.com/v2/CheckConnection',
-            'severity' => 0,
-          ],
-          'cdt.reference_data_api_endpoint' => [
-            'value' => 'https://example.com/v2/requests/businessReferenceData',
-            'severity' => 0,
-          ],
-          'cdt.validate_api_endpoint' => [
-            'value' => 'https://example.com/v2/requests/validate',
-            'severity' => 0,
-          ],
-          'cdt.requests_api_endpoint' => [
-            'value' => 'https://example.com/v2/requests',
-            'severity' => 0,
-          ],
-          'cdt.identifier_api_endpoint' => [
-            'value' => 'https://example.com/v2/requests/requestIdentifierByCorrelationId/:correlationId',
-            'severity' => 0,
-          ],
-          'cdt.status_api_endpoint' => [
-            'value' => 'https://example.com/v2/requests/:requestyear/:requestnumber',
-            'severity' => 0,
-          ],
-          'cdt.username' => [
-            'value' => 'test_username',
-            'severity' => 0,
-          ],
-          'cdt.client' => [
-            'value' => 'test_client',
-            'severity' => 0,
-          ],
-          'cdt.password' => [
-            'value' => 'Value set',
-            'severity' => 0,
-          ],
-        ],
-      ],
-      'missing settings' => [
-        [
-          'cdt.token_api_endpoint' => 'https://example.com/token',
-        ],
-        [
-          'cdt.token_api_endpoint' => [
-            'value' => 'https://example.com/token',
-            'severity' => 0,
-          ],
-          'cdt.main_api_endpoint' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.reference_data_api_endpoint' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.validate_api_endpoint' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.requests_api_endpoint' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.identifier_api_endpoint' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.status_api_endpoint' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.username' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.client' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-          'cdt.password' => [
-            'value' => 'Value not set',
-            'severity' => 1,
-          ],
-        ],
-      ],
-    ];
+    // Test missing settings.
+    new Settings([]);
+    $requirements = oe_translation_cdt_requirements('runtime');
+    foreach ($settings as $name => $value) {
+      $this->assertArrayHasKey($name, $requirements);
+      $this->assertEquals('Value not set', $requirements[$name]['value']);
+      $this->assertEquals(1, $requirements[$name]['severity']);
+    }
   }
 
 }
