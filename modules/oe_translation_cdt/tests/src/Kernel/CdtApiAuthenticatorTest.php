213caed52e6ef01e3a52bd75532753bbbbe52c01
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-04-09 08:39:18 +0200

    D8AGE-425: Update tests of CDT Api Authenticator.

--- modules/oe_translation_cdt/tests/src/Kernel/CdtApiAuthenticatorTest.php
+++ modules/oe_translation_cdt/tests/src/Kernel/CdtApiAuthenticatorTest.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 use Drupal\Component\Datetime\Time;
 use Drupal\Core\State\StateInterface;
-use Drupal\oe_translation_cdt\Api\ApiAuthenticator;
+use Drupal\oe_translation_cdt\Api\CdtApiAuthenticator;
 use Drupal\oe_translation_cdt\Exception\CdtConnectionException;
 use Drupal\Tests\oe_translation\Kernel\TranslationKernelTestBase;
-use OpenEuropa\CdtClient\ApiClient;
+use OpenEuropa\CdtClient\Contract\ApiClientInterface;
 use OpenEuropa\CdtClient\Model\Response\Token;
 use Prophecy\Prophecy\ObjectProphecy;
 
@@ -16,7 +16,7 @@ use Prophecy\Prophecy\ObjectProphecy;
  *
  * @group batch1
  */
-class ApiAuthenticatorTest extends TranslationKernelTestBase {
+class CdtApiAuthenticatorTest extends TranslationKernelTestBase {
 
   /**
    * {@inheritdoc}
@@ -29,7 +29,7 @@ class ApiAuthenticatorTest extends TranslationKernelTestBase {
   /**
    * The API client prophecy.
    */
-  private ApiClient|ObjectProphecy $apiProphecy;
+  private ApiClientInterface|ObjectProphecy $apiProphecy;
 
   /**
    * The token.
@@ -39,7 +39,7 @@ class ApiAuthenticatorTest extends TranslationKernelTestBase {
   /**
    * The authenticator.
    */
-  private ApiAuthenticator $authenticator;
+  private CdtApiAuthenticator $authenticator;
 
   /**
    * The state.
@@ -55,16 +55,19 @@ class ApiAuthenticatorTest extends TranslationKernelTestBase {
     $time_prophecy = $this->prophesize(Time::class);
     $time_prophecy->getCurrentTime()->willReturn(100);
     $this->container->set('datetime.time', $time_prophecy->reveal());
-    $this->apiProphecy = $this->prophesize(ApiClient::class);
+    $this->apiProphecy = $this->prophesize(ApiClientInterface::class);
     $this->container->set('oe_translation_cdt.api_client', $this->apiProphecy->reveal());
 
     $this->token = (new Token())
       ->setAccessToken('abc')
       ->setExpiresIn(200)
       ->setTokenType('Bearer');
-    $this->apiProphecy->setToken($this->token)->willReturn($this->apiProphecy->reveal());
+    $this->apiProphecy
+      ->setToken($this->token)
+      ->willReturn($this->apiProphecy->reveal());
 
     $this->authenticator = $this->container->get('oe_translation_cdt.api_authenticator');
+    $this->authenticator->resetAuthentication();
     $this->state = $this->container->get('state');
   }
 
@@ -72,42 +75,70 @@ class ApiAuthenticatorTest extends TranslationKernelTestBase {
    * Test the valid authentication with the token request.
    */
   public function testValidRequestAuthentication(): void {
-    $this->apiProphecy->requestToken()->shouldBeCalledOnce()->willReturn($this->token);
-    $this->apiProphecy->checkConnection()->willReturn(TRUE)->shouldBeCalledOnce();
+    $this->apiProphecy->requestToken()
+      ->shouldBeCalledOnce()
+      ->willReturn($this->token);
+    $this->apiProphecy->checkConnection()
+      ->willReturn(TRUE)
+      ->shouldBeCalledOnce();
     $this->authenticator->authenticate();
     $this->assertEquals($this->state->get('cdt.token'), serialize($this->token));
     $this->assertEquals($this->state->get('cdt.token_expiry_date'), 300);
   }
 
   /**
-   * Test the valid authentication with stored token.
+   * Test the valid authentication with the stored token.
    */
   public function testValidStoredAuthentication(): void {
     $this->state->set('cdt.token', serialize($this->token));
     $this->state->set('cdt.token_expiry_date', 300);
     $this->apiProphecy->requestToken()->shouldNotBeCalled();
-    $this->apiProphecy->checkConnection()->willReturn(TRUE)->shouldBeCalledOnce();
+    $this->apiProphecy->checkConnection()
+      ->willReturn(TRUE)
+      ->shouldBeCalledOnce();
     $this->authenticator->authenticate();
   }
 
   /**
-   * Test the failed authentication with invalid stored token.
+   * Test the authentication with invalid stored token.
    */
   public function testInvalidStoredAuthentication(): void {
-    // @todo Finish and create test for expired token.
     $this->state->set('cdt.token', serialize($this->token));
     $this->state->set('cdt.token_expiry_date', 300);
-    $this->apiProphecy->requestToken()->shouldNotBeCalled();
-    $this->apiProphecy->checkConnection()->willReturn(TRUE)->shouldBeCalledOnce();
+    $this->apiProphecy->requestToken()
+      ->shouldBeCalledOnce()
+      ->willReturn($this->token);
+    $this->apiProphecy->checkConnection()
+      ->willReturn(FALSE, TRUE)
+      ->shouldBeCalledTimes(2);
     $this->authenticator->authenticate();
   }
 
   /**
-   * Test the authentication with the token request and failed checkConnection.
+   * Test the authentication with expired stored token.
+   */
+  public function testExpiredStoredAuthentication(): void {
+    $this->state->set('cdt.token', serialize($this->token));
+    $this->state->set('cdt.token_expiry_date', 50);
+    $this->apiProphecy->requestToken()
+      ->shouldBeCalledOnce()
+      ->willReturn($this->token);
+    $this->apiProphecy->checkConnection()
+      ->willReturn(TRUE)
+      ->shouldBeCalledOnce();
+    $this->authenticator->authenticate();
+  }
+
+  /**
+   * Test the authentication with the valid token, but failed connection.
    */
   public function testInvalidConnection(): void {
-    $this->apiProphecy->requestToken()->shouldBeCalledOnce()->willReturn($this->token);
-    $this->apiProphecy->checkConnection()->willReturn(FALSE)->shouldBeCalledOnce();
+    $this->apiProphecy->requestToken()
+      ->shouldBeCalledOnce()
+      ->willReturn($this->token);
+    $this->apiProphecy->checkConnection()
+      ->willReturn(FALSE)
+      ->shouldBeCalledOnce();
     $this->expectExceptionObject(new CdtConnectionException('The connection to the CDT API could not be established.'));
     $this->authenticator->authenticate();
   }
@@ -117,8 +148,12 @@ class ApiAuthenticatorTest extends TranslationKernelTestBase {
    */
   public function testMultipleAuthenticationAttempts(): void {
     $this->authenticator->resetAuthentication();
-    $this->apiProphecy->requestToken()->shouldBeCalledOnce()->willReturn($this->token);
-    $this->apiProphecy->checkConnection()->willReturn(TRUE)->shouldBeCalledOnce();
+    $this->apiProphecy->requestToken()
+      ->shouldBeCalledOnce()
+      ->willReturn($this->token);
+    $this->apiProphecy->checkConnection()
+      ->willReturn(TRUE)
+      ->shouldBeCalledOnce();
     $this->authenticator->authenticate();
     $this->authenticator->authenticate();
   }
