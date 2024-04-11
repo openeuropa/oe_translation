65d7012a1f5ad08eedfa136aadd35d4559e6f9fc
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-04-11 14:59:52 +0200

    D8AGE-425: Use compiler pass to set Container parameters.

--- modules/oe_translation_cdt/src/OeTranslationCdtServiceProvider.php
+++ modules/oe_translation_cdt/src/OeTranslationCdtServiceProvider.php
@@ -4,10 +4,10 @@ namespace Drupal\oe_translation_cdt;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
-use Drupal\Core\Site\Settings;
+use Drupal\oe_translation_cdt\Compiler\CdtParametersCompilerPass;
 
 /**
- * Updates the container with CDT client parameters.
+ * Runs the custom compiler pass.
  */
 class OeTranslationCdtServiceProvider extends ServiceProviderBase {
 
@@ -15,19 +15,7 @@ class OeTranslationCdtServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function register(ContainerBuilder $container): void {
-    $container->setParameter('cdt.client_configuration', [
-      'mainApiEndpoint' => Settings::get('cdt.main_api_endpoint'),
-      'tokenApiEndpoint' => Settings::get('cdt.token_api_endpoint'),
-      'referenceDataApiEndpoint' => Settings::get('cdt.reference_data_api_endpoint'),
-      'validateApiEndpoint' => Settings::get('cdt.validate_api_endpoint'),
-      'requestsApiEndpoint' => Settings::get('cdt.requests_api_endpoint'),
-      'identifierApiEndpoint' => Settings::get('cdt.identifier_api_endpoint'),
-      'statusApiEndpoint' => Settings::get('cdt.status_api_endpoint'),
-      'username' => Settings::get('cdt.username'),
-      'password' => Settings::get('cdt.password'),
-      'client' => Settings::get('cdt.client'),
-      'api_key' => Settings::get('cdt.api_key'),
-    ]);
+    $container->addCompilerPass(new CdtParametersCompilerPass());
   }
 
 }
