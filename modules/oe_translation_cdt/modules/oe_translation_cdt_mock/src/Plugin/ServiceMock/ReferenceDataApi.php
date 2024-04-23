6357ab390e73dbcc6e490bbf9ded1836a889dfaf
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-04-23 16:27:14 +0200

    D8AGE-426: The Reference Data API mock.

---
+++ modules/oe_translation_cdt/modules/oe_translation_cdt_mock/src/Plugin/ServiceMock/ReferenceDataApi.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\oe_translation_cdt_mock\Plugin\ServiceMock;
+
+use Drupal\Core\Site\Settings;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Intercepts reference data request made to CDT.
+ *
+ * @ServiceMock(
+ *   id = "oe_translation_cdt_reference_data_api",
+ *   label = @Translation("CDT mocked reference data responses for testing."),
+ *   weight = -1,
+ * )
+ */
+class ReferenceDataApi extends ServiceMockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEndpointUrl(): string {
+    return Settings::get('cdt.reference_data_api_endpoint');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResponse(RequestInterface $request, array $options): ResponseInterface {
+    if (!$this->hasToken($request)) {
+      return new Response(401, [], $this->getResponseFromFile('general_response_401.json'));
+    }
+    return new Response(200, [], $this->getResponseFromFile('reference_data_response_200.json'));
+  }
+
+}
