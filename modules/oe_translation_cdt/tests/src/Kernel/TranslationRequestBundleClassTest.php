cf9430abb33defdd34f8a3cab454847d67a3797e
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-04-26 15:31:15 +0200

    D8AGE-291: Move the status conversion to the updater.

--- modules/oe_translation_cdt/tests/src/Kernel/TranslationRequestBundleClassTest.php
+++ modules/oe_translation_cdt/tests/src/Kernel/TranslationRequestBundleClassTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Drupal\oe_translation\Entity\TranslationRequest;
 use Drupal\oe_translation_cdt\TranslationRequestCdtInterface;
-use Drupal\oe_translation_remote\TranslationRequestRemoteInterface;
 use Drupal\Tests\oe_translation\Kernel\TranslationKernelTestBase;
 
 /**
@@ -45,12 +44,6 @@ class TranslationRequestBundleClassTest extends TranslationKernelTestBase {
 
     $request->setCdtId('test_cdt_id');
     $this->assertEquals('test_cdt_id', $request->getCdtId());
-
-    $request->setRequestStatusFromCdt('COMP');
-    $this->assertEquals(TranslationRequestRemoteInterface::STATUS_REQUEST_TRANSLATED, $request->getRequestStatus());
-
-    $request->updateTargetLanguageStatusFromCdt('en', 'CMP');
-    $this->assertEquals(TranslationRequestRemoteInterface::STATUS_LANGUAGE_REVIEW, $request->getTargetLanguage('en')?->getStatus());
 
     $request->setComments('test_comments');
     $this->assertEquals('test_comments', $request->getComments());
