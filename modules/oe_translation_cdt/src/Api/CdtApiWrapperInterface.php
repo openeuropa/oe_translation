bdef8236cc91b8adad7e957cd404def8ecc2b15d
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-05-14 09:10:30 +0200

    D8AGE-291: Move statuses to constants.

--- modules/oe_translation_cdt/src/Api/CdtApiWrapperInterface.php
+++ modules/oe_translation_cdt/src/Api/CdtApiWrapperInterface.php
@@ -11,6 +11,18 @@ use OpenEuropa\CdtClient\Contract\ApiClientInterface;
  */
 interface CdtApiWrapperInterface {
 
+  const STATUS_JOB_COMPLETED = 'CMP';
+  const STATUS_JOB_FAILED = 'FLR';
+  const STATUS_JOB_IN_PROGRESS = 'INP';
+  const STATUS_JOB_CANCELLED = 'CNC';
+  const STATUS_JOB_TO_BE_CANCELLED = 'TCN';
+
+  const STATUS_REQUEST_COMPLETED = 'COMP';
+  const STATUS_REQUEST_IN_PROGRESS = 'INPR';
+  const STATUS_REQUEST_CANCELLED = 'CANC';
+  const STATUS_REQUEST_PENDING_APPROVAL = 'PEND';
+  const STATUS_REQUEST_UNDER_QUOTATION = 'UNDE';
+
   /**
    * Gets and authenticates the client.
    *
