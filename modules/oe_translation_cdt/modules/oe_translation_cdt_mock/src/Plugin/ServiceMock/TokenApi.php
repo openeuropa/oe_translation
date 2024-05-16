331c032ffff9743d1a506532f568ba0bd7881999
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-05-16 09:23:24 +0200

    D8AGE-291: Changed API weights to avoid conflicts with the http mock module.

--- modules/oe_translation_cdt/modules/oe_translation_cdt_mock/src/Plugin/ServiceMock/TokenApi.php
+++ modules/oe_translation_cdt/modules/oe_translation_cdt_mock/src/Plugin/ServiceMock/TokenApi.php
@@ -15,7 +15,7 @@ use Psr\Http\Message\ResponseInterface;
  * @ServiceMock(
  *   id = "oe_translation_cdt_token_api",
  *   label = @Translation("CDT mocked token responses for testing."),
- *   weight = -1,
+ *   weight = -2,
  * )
  */
 class TokenApi extends ServiceMockBase {
