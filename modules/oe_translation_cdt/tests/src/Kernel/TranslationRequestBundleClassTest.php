cc8b3c9198d195ef064dbef080af308665d2069b
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-05-16 08:28:42 +0200

    D8AGE-291: Add namespace to tests.

--- modules/oe_translation_cdt/tests/src/Kernel/TranslationRequestBundleClassTest.php
+++ modules/oe_translation_cdt/tests/src/Kernel/TranslationRequestBundleClassTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Drupal\Tests\oe_translation_cdt\Kernel;
+
 use Drupal\oe_translation\Entity\TranslationRequest;
 use Drupal\oe_translation_cdt\TranslationRequestCdtInterface;
 use Drupal\Tests\oe_translation\Kernel\TranslationKernelTestBase;
