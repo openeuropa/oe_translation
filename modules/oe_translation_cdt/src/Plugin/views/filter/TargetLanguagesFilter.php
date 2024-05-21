7598ce4cc6a054faec31afa7b983d5a050ffe6e6
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-05-21 12:57:37 +0200

    D8AGE-341: Additional view fields.

---
+++ modules/oe_translation_cdt/src/Plugin/views/filter/TargetLanguagesFilter.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\oe_translation_cdt\Plugin\views\filter;
+
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Filters by the target languages.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("oe_translation_cdt_target_languages_filter")
+ */
+class TargetLanguagesFilter extends InOperator {
+
+  /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $languageManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->languageManager = $languageManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('language_manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValueOptions() {
+    $languages = $this->languageManager->getLanguages();
+    foreach ($languages as $language) {
+      $this->valueOptions[$language->getId()] = $language->getName();
+    }
+
+    return $this->valueOptions;
+  }
+
+}
