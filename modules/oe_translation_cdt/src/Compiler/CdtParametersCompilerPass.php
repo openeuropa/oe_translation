743196951c08421fc8653908485b4eb76d77577c
Grzegorz Pietrzak <grzegorz.pietrzak@gmail.com>
2024-07-25 09:55:00 +0200

    D8AGE-542: Escaping of percent characters in the compiler pass.

--- modules/oe_translation_cdt/src/Compiler/CdtParametersCompilerPass.php
+++ modules/oe_translation_cdt/src/Compiler/CdtParametersCompilerPass.php
@@ -21,10 +21,10 @@ class CdtParametersCompilerPass implements CompilerPassInterface {
     // Array values don't support the "%" character, that may
     // appear in the password or in the URL.
     // @see https://www.drupal.org/project/drupal/issues/3458344
-    $container->setParameter('cdt.base_api_url', Settings::get('cdt.base_api_url'));
-    $container->setParameter('cdt.username', Settings::get('cdt.username'));
-    $container->setParameter('cdt.password', Settings::get('cdt.password'));
-    $container->setParameter('cdt.client', Settings::get('cdt.client'));
+    $container->setParameter('cdt.base_api_url', str_replace("%", "%%", Settings::get('cdt.base_api_url', '')));
+    $container->setParameter('cdt.username', str_replace("%", "%%", Settings::get('cdt.username', '')));
+    $container->setParameter('cdt.password', str_replace("%", "%%", Settings::get('cdt.password', '')));
+    $container->setParameter('cdt.client', str_replace("%", "%%", Settings::get('cdt.client', '')));
   }
 
 }
